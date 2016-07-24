21ec9fac0f0cf3e7af857251f9ca727c351d35f4
hhkk <hank.khang@gmail.com>
2016-07-24 05:05:15 -0400

    savb

--- client/app.ts
+++ client/app.ts
@@ -1,13 +1,26 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Parties }   from '../collections/parties';
+import { Mongo }     from 'meteor/mongo';
+import { PartiesForm } from './imports/parties-form/parties-form';
 
 import template from './app.html';
 
 @Component({
     selector: 'app',
-    template
+    template,
+    directives: [PartiesForm]
 })
-class Socially { }
+class Socially {
+    parties: Mongo.Cursor<Object>;
+
+    constructor () {
+        this.parties = Parties.find();
+    }
+    removeParty(party) {
+        Parties.remove(party._id);
+    }
+}
 
 bootstrap(Socially);
